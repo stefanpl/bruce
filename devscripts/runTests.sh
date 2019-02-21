4e263db8ef49f7747857a53511bb5bca30883675
Stefan Plösser <stefan@rarelime.de>
2019-02-21 21:14:19 +0100

    source .env.docker to have the stack name in runTests.sh

--- devscripts/runTests.sh
+++ devscripts/runTests.sh
@@ -4,6 +4,7 @@ set -e
 scriptDirectory=$( dirname ${BASH_SOURCE[0]} )
 BASH_UTILS_LOCATION=${scriptDirectory}/../node_modules/bash-utils/utils
 source ${BASH_UTILS_LOCATION}/docker/docker_find_container_id.sh
+source ${scriptDirectory}/../docker/.env.docker
 
 containerId=`docker_find_container_id ${STACK_NAME}_nodeserver`
 
