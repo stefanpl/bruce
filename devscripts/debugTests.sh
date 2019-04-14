f760d70d42500faec7e8b24317db50bf8fdd6b1d
Stefan Plösser <stefan@rarelime.de>
2019-04-14 12:43:48 +0200

    debugging with --detach flag

--- devscripts/debugTests.sh
+++ devscripts/debugTests.sh
@@ -3,4 +3,13 @@ set -e
 
 source $( dirname ${BASH_SOURCE[0]} )/utils.sh
 
-runCommandOnRunningNodeContainer "npm run test-debug"
+containerId=`docker_find_container_id ${STACK_NAME}_nodeserver`
+
+# First, kill any running test processes
+set +e
+docker exec --interactive --user node --workdir /home/node/app ${containerId} /bin/bash -c \
+"pkill --full 9231"
+set -e
+
+docker exec --detach --interactive --user node --workdir /home/node/app ${containerId} /bin/bash -c \
+"npm run test-debug"
