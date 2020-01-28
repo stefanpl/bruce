9e91023619a139db8d57153dded5a0299bcc0742
Stefan Plösser <stefan@rarelime.de>
2020-01-28 21:03:52 +0100

    stylistic changes

--- src/validateDataAgainstJsonSchema.ts
+++ src/validateDataAgainstJsonSchema.ts
@@ -8,11 +8,9 @@ interface JSONSchemaValidationResult {
 const ajv = new Ajv({});
 
 export async function validateDataAgainstJsonSchema(data: any, schema: JSONSchema7): Promise<JSONSchemaValidationResult> {
-
   const validate = ajv.compile(schema);
   const isValid = await validate(data);
   return {
     isValid: !!isValid
   }
-
 }
